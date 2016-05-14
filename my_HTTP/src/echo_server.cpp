90ca4d83d6d1b9a3f0c741d624ec8d17c21a5d6b
yw <240083998@qq.com>
2016-05-14 23:39:55 +0800

    测试回射服务器代码

---
+++ my_HTTP/src/echo_server.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: echo_server.cpp
+	> Author:  YinWen
+	> Mail: yinwenatbit@163.com
+	> Created Time: Sat 14 May 2016 08:09:14 PM CST
+ ************************************************************************/
+
+#include "../TCPServer/TCPServer.h"
+#include "../EventLoop/Epoll.h"
+#include "../Protocol/Echo.h"
+
+int main(int argc, char * argv[])
+{
+    TCPServer<Echo> EchoServer(8080, 5);
+    EchoServer.run();
+}
+

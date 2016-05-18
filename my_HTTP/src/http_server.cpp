cceddf4a253bdb51f375580ec180f47a7cc6a3e0
yw <240083998@qq.com>
2016-05-19 01:44:50 +0800

    HTTP服务器启动主函数

---
+++ my_HTTP/src/http_server.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: http_server.cpp
+	> Author:  YinWen
+	> Mail: yinwenatbit@163.com
+	> Created Time: Wed 18 May 2016 10:15:21 PM CST
+ ************************************************************************/
+
+
+#include "../TCPServer/TCPServer.h"
+#include "../EventLoop/Epoll.h"
+#include "../Protocol/HTTP.h"
+
+int main(int argc, char * argv[])
+{
+    TCPServer<HTTP> HTTPServer(8080, 5);
+    HTTPServer.run();
+}
+
